ea20c3d97be09bc74e9a558ef102d399b9506e5c
cmsulzbeck <cmsulzbeck@hotmail.com>
2021-04-22 15:38:34 -0300

    Task, Paginação e Filtro

--- SmartSchool.WebAPI/Data/IRepository.cs
+++ SmartSchool.WebAPI/Data/IRepository.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SmartSchool.WebAPI.Helpers;
 using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Data
@@ -10,7 +13,8 @@ namespace SmartSchool.WebAPI.Data
          bool SaveChanges();
 
         // ALUNOS
-         Aluno[] GetAllAlunos(bool includeProfessor);
+         Task<PageList<Aluno>> GetAllAlunosAsync(PageParams pageParams, bool includeProfessor = false);
+         Aluno[] GetAllAlunos(bool includeProfessor = false);
          Aluno[] GetAllAlunosByDisciplinaId(int disciplinaId, bool includeProfessor);
          Aluno GetAlunoById(int alunoId, bool includeProfessor);
 
--- SmartSchool.WebAPI/Data/Repository.cs
+++ SmartSchool.WebAPI/Data/Repository.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Helpers;
 using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Data
@@ -47,6 +50,34 @@ namespace SmartSchool.WebAPI.Data
             query = query.AsNoTracking().OrderBy(a => a.Id);
 
             return query.ToArray();
+        }
+
+        public async Task<PageList<Aluno>> GetAllAlunosAsync(PageParams pageParams, bool includeProfessor = false)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            // Caso usuário queria retornar as disciplinas do aluno junto do Aluno, incluir as informações da disciplina e do professor
+            if(includeProfessor)
+            {
+                query = query.Include(a => a.AlunosDisciplinas)
+                             .ThenInclude(ad => ad.Disciplina)
+                             .ThenInclude(d => d.Professor);
+            }
+
+            query = query.AsNoTracking().OrderBy(a => a.Id);
+
+            if(!string.IsNullOrEmpty(pageParams.Nome))
+                query = query.Where(aluno => aluno.Nome.ToUpper().Contains(pageParams.Nome.ToUpper()) ||
+                                             aluno.Sobrenome.ToUpper().Contains(pageParams.Nome.ToUpper()));
+
+            if(pageParams.Matricula > 0)
+                query = query.Where(aluno => aluno.Matricula == pageParams.Matricula);
+
+            if(pageParams.Ativo != null)
+                query = query.Where(aluno => aluno.Ativo == pageParams.Ativo);
+
+            // return await query.ToListAsync();
+            return await PageList<Aluno>.CreateAsync(query, pageParams.PageNumber, pageParams.PageSize);
         }
 
         public Aluno[] GetAllAlunosByDisciplinaId(int disciplinaId, bool includeProfessor = false)
